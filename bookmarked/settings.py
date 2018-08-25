ab54aa25abdd81435e0b93250506eeae78e3acda
sid22 <siddharth.game@gmail.com>
2018-08-25 23:37:55 +0530

    fixed all settings

--- bookmarked/settings.py
+++ bookmarked/settings.py
@@ -14,8 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '^szzok3^(x%nrcd8okvy-an$9_lfbma6fu3x3ezna-*mtdu870'
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -66,71 +64,20 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'bookmarked.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 
-# MONGO_URI = 'localhost'
-# MONGO_PORT = 27017
-
-# MONGO_USER = 'batman22'
-# MONGO_PASSWORD = 'batman22'
-
-# MONGO_URI = 'localhost'
-# MONGO_PORT = 27017
 
 if os.environ.get('ENVIRONMENT') == None:
     MONGO_URL = 'localhost'
     MONGO_PORT = str(27017)
     MONGO_URI = "mongodb://" + MONGO_URL + ":" + MONGO_PORT + "/"
     DEBUG = True
+    SECRET_KEY = 'abcdef'
 elif os.environ.get('ENVIRONMENT') == 'production':
     MONGO_USER = os.environ['MONGO_USER']
     MONGO_PASSWORD = os.environ['MONGO_PASSWORD']
@@ -139,8 +86,4 @@ elif os.environ.get('ENVIRONMENT') == 'production':
     MONGO_DBNAME = os.environ['MONGO_DBNAME']
     MONGO_URI = "mongodb://" + MONGO_USER + ":" + MONGO_PASSWORD + "@" + MONGO_URL + ":" + MONGO_PORT + "/" + MONGO_DBNAME
     DEBUG = False
-# MONGO_URL = 'localhost'
-# MONGO_PORT = str(27017)
-# MONGO_URI = "mongodb://" + MONGO_URL + ":" + MONGO_PORT + "/"
-
-# MONGO_URI = "mongodb://batman22:batman22@ds022228.mlab.com:22228/bookmarked"
+    SECRET_KEY = os.environ['SECRET_KEY']
